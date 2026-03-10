99ea785baa2785ccfad83236d2ca31a8875ab50a
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-03-10 18:49:34 -0300

    Add update and delete methods for CourseService

--- src/main/java/tech/ada/service/CourseService.java
+++ src/main/java/tech/ada/service/CourseService.java
@@ -2,10 +2,10 @@ package tech.ada.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import tech.ada.dto.CourseRequestDTO;
 import tech.ada.model.Course;
-
 import java.util.List;
 
 @ApplicationScoped
@@ -17,15 +17,36 @@ public class CourseService {
     public Course getCourseById(Long id) {
         Course course = Course.findById(id);
         if (course == null) {
-            throw new EntityNotFoundException("Course with id " + id + "was not found");
+            throw new EntityNotFoundException("Course with id " + id + " not found");
         }
         return course;
     }
 
+    @Transactional
     public Course createCourse(@Valid CourseRequestDTO dto) {
         Course course = new Course(dto.name());
         course.persist();
         return course;
 
     }
+
+    @Transactional
+    public Course updateCourse(Long id, @Valid CourseRequestDTO dto) {
+        Course course = Course.findById(id);
+        if (course == null) {
+            throw new EntityNotFoundException("Course with id " + id + " not found");
+        }
+        course.changeName(dto.name());
+        return course;
+    }
+
+   @Transactional
+   public Course deleteCourse(Long id, @Valid CourseRequestDTO dto) {
+       Course course = Course.findById(id);
+       if (course == null) {
+           throw new EntityNotFoundException("Course with id " + id + " not found");
+       }
+       course.delete();
+       return course;
+   }
 }
