43fdd21e903b6a89dce8f20e0dccf50bc3250087
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-03-11 00:33:24 -0300

    Add get, put, and delete by Id in resource

--- src/main/java/tech/ada/resource/CourseResource.java
+++ src/main/java/tech/ada/resource/CourseResource.java
@@ -1,8 +1,6 @@
 package tech.ada.resource;
 
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -15,27 +13,25 @@ import java.net.URI;
 import java.util.List;
 
 @Path("/courses")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class CourseResource {
     @Inject
     CourseService service;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public Response getCourses() {
         List<Course> courses = service.getCourses();
 
-        List<CourseResponseDTO> response = courses.stream()
+        List<CourseResponseDTO> payload = courses.stream()
                 .map((Course c) -> new CourseResponseDTO(c.getId(),
                                                          c.getName()))
                 .toList();
-        return Response.ok(response).build();
+        return Response.ok(payload).build();
     }
 
     @POST
-    @Transactional
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response createCourse(@Valid CourseRequestDTO dto) {
+    public Response createCourse(CourseRequestDTO dto) {
         Course course = service.createCourse(dto);
 
         URI location = URI.create("/courses/" + course.getId());
@@ -45,5 +41,30 @@ public class CourseResource {
                 .header("Content-Type", "application/json")
                 .entity(payload)
                 .build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getCourseById(@PathParam("id") Long id) {
+        Course course = service.getCourseById(id);
+        CourseResponseDTO payload = new CourseResponseDTO(id, course.getName());
+        return Response.ok(payload).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response updateCourse(@PathParam("id") Long id,
+                                 CourseRequestDTO dto) {
+        Course course = service.updateCourse(id, dto);
+        CourseResponseDTO payload = new CourseResponseDTO(id, course.getName());
+        return Response.ok(payload).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteCourse(@PathParam("id") Long id,
+                                 CourseRequestDTO dto) {
+        service.deleteCourse(id, dto);
+        return Response.noContent().build();
     }
 }
